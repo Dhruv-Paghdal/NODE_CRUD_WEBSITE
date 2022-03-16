62828b2f992b13153fe71706666982261dbe3407
Dhruv-Paghdal <85886757+Dhruv-Paghdal@users.noreply.github.com>
2022-03-16 12:34:58 +0530

    Add files via upload

---
+++ apiCall.js
@@ -0,0 +1,23 @@
+const host=`http://localhost:${process.env.PORT}`;
+
+exports.deleteProduct=async (id)=>{
+    const url=`${host}/backend/editProduct/deleteProduct/${id}`;
+    const response = await fetch(url, {
+      method: 'DELETE', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    window.location.href=response.url;
+}
+
+exports.updateProduct=async (id)=>{
+    const url=`${host}/backend/editProduct/updateProduct/${id}`; 
+    const response = await fetch(url, {
+      method: 'GET', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    window.location.href=response.url;
+}
