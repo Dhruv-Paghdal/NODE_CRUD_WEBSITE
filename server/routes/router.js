62e7abf9f1e76dd2f825aa2e9cc934e98519c082
Dhruv-Paghdal <85886757+Dhruv-Paghdal@users.noreply.github.com>
2022-03-16 11:21:01 +0530

    Update router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,25 +4,26 @@ const backedController = require('../controller/backedController');
 const frontendController = require('../controller/frontendController');
 const store = require('../middleware/multer');
 const cookie=require('../middleware/cookieChecker');
+const cors=require('../middleware/cors');
 
 // Frontend Routes
-route.get("/",frontendController.home);
-route.get("/services",frontendController.services);
-route.get("/products",frontendController.products);
-route.get("/about",frontendController.about);
-route.post("/sendEnquiry",frontendController.sendEnquiry);
+route.get("/",cors,frontendController.home);
+route.get("/services",cors,frontendController.services);
+route.get("/products",cors,frontendController.products);
+route.get("/about",cors,frontendController.about);
+route.post("/sendEnquiry",cors,frontendController.sendEnquiry);
 
 // Backend Routes
-route.get("/backend",backedController.backend);
-route.post("/loginCheck",backedController.loginCheck);
-route.get("/backend/home",cookie,backedController.backendHome);
-route.get("/backend/addProduct",cookie,backedController.addProduct);
-route.get("/backend/editProduct",cookie,backedController.editProduct);
-route.post("/sendData",store.array('productImg'),cookie,backedController.sendData);
-route.post("/backend/editProduct/selectProduct",cookie,backedController.selectProduct);
-route.delete("/backend/editProduct/deleteProduct/:id",cookie,backedController.deleteProduct);
-route.get("/backend/editProduct/updateProduct/:id",cookie,backedController.updateProduct);
-route.post("/sendUpdate/:id",store.array('productImg'),cookie,backedController.sendUpdate);
-route.get("/backend/logout",backedController.logout);
+route.get("/backend",cors,backedController.backend);
+route.post("/loginCheck",cors,backedController.loginCheck);
+route.get("/backend/home",cookie,cors,backedController.backendHome);
+route.get("/backend/addProduct",cookie,cors,backedController.addProduct);
+route.get("/backend/editProduct",cookie,cors,backedController.editProduct);
+route.post("/sendData",store.array('productImg'),cookie,cors,backedController.sendData);
+route.post("/backend/editProduct/selectProduct",cookie,cors,backedController.selectProduct);
+route.delete("/backend/editProduct/deleteProduct/:id",cookie,cors,backedController.deleteProduct);
+route.get("/backend/editProduct/updateProduct/:id",cookie,cors,backedController.updateProduct);
+route.post("/sendUpdate/:id",store.array('productImg'),cookie,cors,backedController.sendUpdate);
+route.get("/backend/logout",cors,backedController.logout);
 
 module.exports=route;
