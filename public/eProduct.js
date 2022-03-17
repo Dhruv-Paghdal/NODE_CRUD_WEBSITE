18d242f21b73dc2de7d3ba465714380f6d676f13
Dhruv-Paghdal <85886757+Dhruv-Paghdal@users.noreply.github.com>
2022-03-17 11:13:46 +0530

    Update eProduct.js

--- public/eProduct.js
+++ public/eProduct.js
@@ -1,23 +1,21 @@
 const deleteProduct=async (hostNumber,id)=>{
     const host=`http://localhost:${hostNumber}`;
     const url=`${host}/backend/editProduct/deleteProduct/${id}`;
-    const response = await fetch(url, {
+    fetch(url, {
       method: 'DELETE', 
       headers: {
         'Content-Type': 'application/json'
       },
-    });
-    window.location.href=response.url;
+    }).then((response)=>window.location.href=response.url);
 }
 
 const updateProduct=async (hostNumber,id)=>{
     const host=`http://localhost:${hostNumber}`;
     const url=`${host}/backend/editProduct/updateProduct/${id}`; 
-    const response = await fetch(url, {
+    fetch(url, {
       method: 'GET', 
       headers: {
         'Content-Type': 'application/json'
       },
-    });
-    window.location.href=response.url;
+    }).then((response)=>window.location.href=response.url);
 }
